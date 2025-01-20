1b8e7a7c2bd1a35a426f76dbd9d5780a75d0a8f8
Yassine Zribi <yassinezribiyassine@gmail.com>
2025-01-20 17:46:24 +0100

    Refactor the datatable Th component

--- src/components/DataTable/Th/index.tsx
+++ src/components/DataTable/Th/index.tsx
@@ -13,41 +13,45 @@ interface ThProps {
 export default function Th({ style, children, sortList, name, onSort }: ThProps) {
   const findSortItem = (name: string) => {
     return sortList.find(sortItem => sortItem.split("-")[0] === name)
-}
-const findSortItemIndex = (name: string) => {
+  }
+  const findSortItemIndex = (name: string) => {
     return sortList.findIndex(sortItem => sortItem.split("-")[0] === name)
-}
+  }
 
-const getSortPriority = (name: string) => {
+  const getSortPriority = (name: string) => {
     const index = findSortItemIndex(name)
     if (index === -1)
-        return null
+      return null
     else
-        return index + 1
-}
+      return index + 1
+  }
 
-const isSorted = (name: string): boolean => {
+  const isSorted = (name: string): boolean => {
     return !!findSortItem(name)
-}
-const isReversed = (name: string): boolean => {
+  }
+  const isReversed = (name: string): boolean => {
     const item = findSortItem(name)
     return item !== undefined && item.split("-")[1] === "desc"
-}
+  }
 
-const priority = getSortPriority(name)
+  const priority = getSortPriority(name)
 
   const Icon = isSorted(name) ? (isReversed(name) ? ArrowLongDownIcon : ArrowLongUpIcon) : ChevronUpDownIcon;
   return (
     <Table.Th className={classes.th} style={style}>
       <UnstyledButton onClick={() => onSort(name)} className={classes.control}>
-        <Group justify="space-between">
+        <Group justify="space-between" wrap='nowrap'>
           <Text fw={500} fz="sm">
-            {children} {priority !== null && <>({priority})</>}
+            {children}
           </Text>
-
-          <Center className={classes.icon}>
-            <Icon style={{ width: rem(16), height: rem(16) }} />
-          </Center>
+          <Group wrap='nowrap' gap={0}>
+            <Text fw={500} fz="sm">
+              {priority !== null && <>({priority})</>}
+            </Text>
+            <Center className={classes.icon}>
+              <Icon style={{ width: rem(16), height: rem(16) }} />
+            </Center>
+          </Group>
         </Group>
       </UnstyledButton>
     </Table.Th>
